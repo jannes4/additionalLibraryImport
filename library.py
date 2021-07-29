e35f2b2e4e91c5dbe8f8bf2a5c93be0f29f5f66b
Jannes <62259070+jannes4@users.noreply.github.com>
2021-07-29 23:16:18 +0200

    Update library.py

--- library.py
+++ library.py
@@ -1,11 +1,1 @@
-import pyautogui
 
-pyautogui.keyDown("win")
-pyautogui.keyDown("ctrl")
-
-for i in range(0, 500):
-    pyautogui.press('d')
-
-while True:
-    pyautogui.keyDown("win")
-    pyautogui.keyDown("ctrl")
